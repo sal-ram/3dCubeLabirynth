83b7e55ea8d5637e003d30b9c844c404c8a9421d
Ramazan Salakhov <64357780+sal-ram@users.noreply.github.com>
2022-04-30 15:56:17 +0500

    more clean code

--- Assets/Scripts/MazeSpawner.cs
+++ Assets/Scripts/MazeSpawner.cs
@@ -23,6 +23,115 @@ public class MazeSpawner : MonoBehaviour
 
     public static int length { get; set; }
 
+    private void SpawnSideOfCube(GameObject Parent, MazeCubeGeneratorCell[,,] maze, int numberOfSide)
+    {
+        switch (numberOfSide)
+        {
+            case 1:
+                for (int x = 1; x < length; x++)
+                {
+                    for (int y = 1; y < length; y++)
+                    {
+                        var FrontChild = Instantiate(_CellPrefab, new Vector3(x * coeff, y * coeff, 0), Quaternion.identity);
+                        FrontChild.transform.SetParent(Parent.transform);
+
+                        children.Add(FrontChild);
+
+                        Cell cell = FrontChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[x, y, 0].WallLeft);
+                        cell.WallBottom.SetActive(maze[x, y, 0].WallBottom);
+                        cell.floor.SetActive(maze[x, y, 0].floor);
+                    }
+                }
+                break;
+            case 2:
+                for (int x = 1; x < length; x++)
+                {
+                    for (int y = 1; y < length; y++)
+                    {
+                        var BackChild = Instantiate(_CellPrefab, new Vector3(x * coeff, y * coeff, length * coeff), Quaternion.Euler(0, 180, 0));
+                        BackChild.transform.SetParent(Parent.transform);
+
+                        children.Add(BackChild);
+
+                        Cell cell = BackChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[x, y, length].WallLeft);
+                        cell.WallBottom.SetActive(maze[x, y, length].WallBottom);
+                        cell.floor.SetActive(maze[x, y, length].floor);
+                    }
+                }
+                break;
+            case 3:
+                for (int z = 1; z < length; z++)
+                {
+                    for (int y = 1; y < length; y++)
+                    {
+                        var LeftChild = Instantiate(_CellPrefab, new Vector3(0, y * coeff, z * coeff), Quaternion.Euler(0, 90, 0));
+                        LeftChild.transform.SetParent(Parent.transform);
+
+                        children.Add(LeftChild);
+
+                        Cell cell = LeftChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[0, y, z].WallLeft);
+                        cell.WallBottom.SetActive(maze[0, y, z].WallBottom);
+                        cell.floor.SetActive(maze[0, y, z].floor);
+                    }
+                }
+                break;
+            case 4:
+                for (int z = 1; z < length; z++)
+                {
+                    for (int y = 1; y < length; y++)
+                    {
+                        var RightChild = Instantiate(_CellPrefab, new Vector3(length * coeff, y * coeff, z * coeff), Quaternion.Euler(0, -90, 0));
+                        RightChild.transform.SetParent(Parent.transform);
+
+                        children.Add(RightChild);
+
+                        Cell cell = RightChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[length, y, z].WallLeft);
+                        cell.WallBottom.SetActive(maze[length, y, z].WallBottom);
+                        cell.floor.SetActive(maze[length, y, z].floor);
+                    }
+                }
+                break;
+            case 5:
+                for (int z = 1; z < length; z++)
+                {
+                    for (int x = 1; x < length; x++)
+                    {
+                        var BottomChild = Instantiate(_CellPrefab, new Vector3(x * coeff, 0, z * coeff), Quaternion.Euler(-90, 0, 0));
+                        BottomChild.transform.SetParent(Parent.transform);
+
+                        children.Add(BottomChild);
+
+                        Cell cell = BottomChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[x, 0, z].WallLeft);
+                        cell.WallBottom.SetActive(maze[x, 0, z].WallBottom);
+                        cell.floor.SetActive(maze[x, 0, z].floor);
+                    }
+                }
+                break;
+            case 6:
+                for (int z = 1; z < length; z++)
+                {
+                    for (int x = 1; x < length; x++)
+                    {
+                        var TopChild = Instantiate(_CellPrefab, new Vector3(x * coeff, length * coeff, z * coeff), Quaternion.Euler(90, 0, 0));
+                        TopChild.transform.SetParent(_TopParent.transform);
+
+                        children.Add(TopChild);
+
+                        Cell cell = TopChild.GetComponent<Cell>();
+                        cell.WallLeft.SetActive(maze[x, length, z].WallLeft);
+                        cell.WallBottom.SetActive(maze[x, length, z].WallBottom);
+                        cell.floor.SetActive(maze[x, length, z].floor);
+                    }
+                }
+                break;
+        }
+    }
+
     public void SpawnMaze()
     {
         parents = new List<GameObject>();
@@ -34,121 +143,43 @@ public class MazeSpawner : MonoBehaviour
         length = maze.GetLength(0) - 2;
 
         // далее спавн каждой грани лабиринта куба (передн€€, задн€€ и тд)
-        for (int x = 1; x < length; x++)
-        {
-            for (int y = 1; y < length; y++)
-            {
-                var FrontChild = Instantiate(_CellPrefab, new Vector3(x * coeff, y * coeff, 0), Quaternion.identity);
-                FrontChild.transform.SetParent(_FrontParent.transform);
-
-                children.Add(FrontChild);
-
-                Cell cell = FrontChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[x, y, 0].WallLeft);
-                cell.WallBottom.SetActive(maze[x, y, 0].WallBottom);
-                cell.floor.SetActive(maze[x, y, 0].floor);
-            }
-        }
+       
+        SpawnSideOfCube(_FrontParent, maze, 1);
 
         _FrontParent.transform.position = new Vector3(0, 0, 0);
 
         parents.Add(_FrontParent);
 
-        for (int x = 1; x < length; x++)
-        {
-            for (int y = 1; y < length; y++)
-            {
-                var BackChild = Instantiate(_CellPrefab, new Vector3(x * coeff, y * coeff, length * coeff), Quaternion.Euler(0, 180, 0));
-                BackChild.transform.SetParent(_BackParent.transform);
-
-                children.Add(BackChild);
-
-                Cell cell = BackChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[x, y, length].WallLeft);
-                cell.WallBottom.SetActive(maze[x, y, length].WallBottom);
-                cell.floor.SetActive(maze[x, y, length].floor);
-            }
-        }
+   
+        SpawnSideOfCube(_BackParent, maze, 2);
 
         _BackParent.transform.position = new Vector3(0.63f, 0, -1.01f);
 
         parents.Add(_BackParent);
 
-        for (int z = 1; z < length; z++)
-        {
-            for (int y = 1; y < length; y++)
-            {
-                var LeftChild = Instantiate(_CellPrefab, new Vector3(0, y * coeff, z * coeff), Quaternion.Euler(0, 90, 0));
-                LeftChild.transform.SetParent(_LeftParent.transform);
 
-                children.Add(LeftChild);
-
-                Cell cell = LeftChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[0, y, z].WallLeft);
-                cell.WallBottom.SetActive(maze[0, y, z].WallBottom);
-                cell.floor.SetActive(maze[0, y, z].floor);
-            }
-        }
+        SpawnSideOfCube(_LeftParent, maze, 3);
 
         _LeftParent.transform.position = new Vector3(0.82f, 0, -0.19f);
 
         parents.Add(_LeftParent);
 
-        for (int z = 1; z < length; z++)
-        {
-            for (int y = 1; y < length; y++)
-            {
-                var RightChild = Instantiate(_CellPrefab, new Vector3(length * coeff, y * coeff, z * coeff), Quaternion.Euler(0, -90, 0));
-                RightChild.transform.SetParent(_RightParent.transform);
 
-                children.Add(RightChild);
-
-                Cell cell = RightChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[length, y, z].WallLeft);
-                cell.WallBottom.SetActive(maze[length, y, z].WallBottom);
-                cell.floor.SetActive(maze[length, y, z].floor);
-            }
-        }
+        SpawnSideOfCube(_RightParent, maze, 4);
 
         _RightParent.transform.position = new Vector3(-0.19f, 0, -0.82f);
 
         parents.Add(_RightParent);
 
-        for (int z = 1; z < length; z++)
-        {
-            for (int x = 1; x < length; x++)
-            {
-                var BottomChild = Instantiate(_CellPrefab, new Vector3(x * coeff, 0, z * coeff), Quaternion.Euler(-90, 0, 0));
-                BottomChild.transform.SetParent(_BottomParent.transform);
-
-                children.Add(BottomChild);
-
-                Cell cell = BottomChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[x, 0, z].WallLeft);
-                cell.WallBottom.SetActive(maze[x, 0, z].WallBottom);
-                cell.floor.SetActive(maze[x, 0, z].floor);
-            }
-        }
+    
+        SpawnSideOfCube(_BottomParent, maze, 5);
 
         _BottomParent.transform.position = new Vector3(0, 0.515f, -0.501f);
 
         parents.Add(_BottomParent);
 
-        for (int z = 1; z < length; z++)
-        {
-            for (int x = 1; x < length; x++)
-            {
-                var TopChild = Instantiate(_CellPrefab, new Vector3(x * coeff, length * coeff, z * coeff), Quaternion.Euler(90, 0, 0));
-                TopChild.transform.SetParent(_TopParent.transform);
-
-                children.Add(TopChild);
-
-                Cell cell = TopChild.GetComponent<Cell>();
-                cell.WallLeft.SetActive(maze[x, length, z].WallLeft);
-                cell.WallBottom.SetActive(maze[x, length, z].WallBottom);
-                cell.floor.SetActive(maze[x, length, z].floor);
-            }
-        }
+     
+        SpawnSideOfCube(_TopParent, maze, 6);
 
         _TopParent.transform.position = new Vector3(0, -0.515f, -0.501f);
 
@@ -170,8 +201,9 @@ public class MazeSpawner : MonoBehaviour
             Destroy(child.gameObject);
         }
     }
+
     // функци€, котора€ случайно устанаваливает Ўарик к которой нужно прийти в лабиринте
-    public void SpawnFinish()
+    private void SpawnFinish()
     {
         GameObject sideOfCube = parents[Random.Range(0, parents.Count)];
 
